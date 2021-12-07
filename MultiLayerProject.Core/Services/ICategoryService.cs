88f6b71fdcc7aa278b086df7ee944753a74ef6d9
eakkuzey <ensar@tursys.com>
2021-12-07 17:17:38 +0300

    Add Repository Design Pattern and Create Service Interface.

---
+++ MultiLayerProject.Core/Services/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MultiLayerProject.Core.Models;
+
+namespace MultiLayerProject.Core.Services
+{
+    public interface ICategoryService:IService<Category>
+    {
+        Task<Category> GetWithProductsByIdAsync(int categoryId);
+    }
+}
