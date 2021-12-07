196832933efef7c4f33b93a1b52373124e2e8798
eakkuzey <ensar@tursys.com>
2021-12-07 17:23:29 +0300

    Add UnitOfWork Pattern.

---
+++ MultiLayerProject.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MultiLayerProject.Core.Repositories;
+
+namespace MultiLayerProject.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        ICategoryRepository Categories { get; }
+        IProductRepository Products { get; }
+
+        Task CommitAsync();
+        void Commit();
+
+    }
+}
