9e1d95cfe386c9144a0a8eed4986e35a7af68869
ensarakkuzey <ensarakkuzey@gmail.com>
2021-12-10 00:20:16 +0300

    .

--- MultiLayerProject.Data/AppDbContext.cs
+++ MultiLayerProject.Data/AppDbContext.cs
@@ -10,7 +10,7 @@ namespace MultiLayerProject.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
         {
-        }
+        } 
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
