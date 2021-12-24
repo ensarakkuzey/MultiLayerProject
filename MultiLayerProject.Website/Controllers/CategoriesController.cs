9f9785017a31621c940fd60ebb2f87233715268e
ensarakkuzey <ensarakkuzey@gmail.com>
2021-12-24 15:25:30 +0300

    Create controller for category.

---
+++ MultiLayerProject.Website/Controllers/CategoriesController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MultiLayerProject.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultiLayerProject.Website.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+
+        public CategoriesController(ICategoryService categoryService, IMapper mapper)
+        {
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
